faf8a1a34f93f1ffe3658c3cc97dcce55841dc3e
Mohamed <20412023101099@science.zu.edu.eg>
2026-02-08 19:32:08 +0200

    Added video-sizes endpoint

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { exec, execFile } = require("node:child_process");
+const { filesize } = require("filesize");
 
-app.get("/api", (req, res) => {
+app.get("/test", (req, res) => {
     res.json({ message: "Hello from the API!" });
+});
+
+function convertSizes(formats) {
+    return formats.map((format) => ({
+        ...format,
+        filesize: filesize(format.filesize),
+    }));
+}
+
+app.get("/api/video-sizes/:videoTag", (req, res) => {
+    const startTime = Date.now();
+    const videoTag = req.params.videoTag;
+
+    const command = `yt-dlp -J --no-warnings ${videoTag} | jq {'id: .id, title: .title, formats: [.formats[] | select(.height != null and .filesize != null) | {format_id, height, filesize}]}'`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing yt-dlp: ${error.message}`);
+            return res
+                .status(500)
+                .json({ error: "Failed to retrieve video formats" });
+        }
+        const endTime = Date.now();
+        const duration = `${endTime - startTime} ms`;
+
+        const parsedData = JSON.parse(stdout);
+
+        parsedData.duration = duration;
+        parsedData.formats = convertSizes(parsedData.formats);
+        
+        res.json(parsedData);
+    });
 });
 
 app.listen(3000, () => {
